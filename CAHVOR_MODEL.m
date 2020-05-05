08836e5df2e89b716035834846dd047423e2a35c
Yuki Itoh <yukiitohand@gmail.com>
2020-05-05 11:31:11 -0400

    methods for deriving internal parameters are added

--- CAHVOR_MODEL.m
+++ CAHVOR_MODEL.m
@@ -13,6 +13,12 @@ classdef CAHVOR_MODEL < handle
         V = []
         O = []
         R = []
+        hc
+        vc
+        hs
+        vs
+        Hdash
+        Vdash
     end
     
     methods
@@ -75,6 +81,87 @@ classdef CAHVOR_MODEL < handle
                 tf = false;
             end
         end
+        
+        function [hc,vc] = get_optical_center_pixel(obj)
+            hc = (obj.A*obj.H'); %./norm(obj.A); (A should be normalized)
+            vc = (obj.A*obj.V'); %./norm(obj.A); (A should be normalized)
+            obj.hc = hc; obj.vc = vc;
+        end
+        
+        function [hs,vs] = get_pixel_focal_length(obj)
+            hs = norm(cross(obj.A,obj.H'));
+            vs = norm(cross(obj.A,obj.V'));
+            obj.hs = hs; obj.vs = vs;
+        end
+        
+        function [Hdash,Vdash,hs,vs,hc,vc] = get_image_plane_unit_vectors(obj)
+            [hc,vc] = obj.get_optical_center_pixel();
+            [hs,vs] = obj.get_pixel_focal_length();
+            Hdash = (obj.H-hc*obj.A) / hs;
+            Vdash = (obj.V-vc*obj.A) / vs;
+            obj.Hdash = Hdash;
+            obj.Vdash = Vdash;
+        end
+        
+        function [p_minus_c] = get_p_minus_c_from_xy(obj,xy)
+            [p_minus_c] = cahv_get_p_minus_c_from_xy_v2(xy,obj);
+        end
+        
+        function [pmc_h,pmc_v] = get_pmc_FOV(obj,h_range,v_range)
+            obj.get_image_plane_unit_vectors();
+            pmc_lrtb   = obj.get_p_minus_c_from_xy(...
+                [ h_range(1) obj.vc    ;
+                  h_range(2) obj.vc    ;
+                  obj.hc     v_range(1);
+                  obj.hc     v_range(2) ]');
+            pmc_h = pmc_lrtb(:,1:2);
+            pmc_v = pmc_lrtb(:,3:4);
+            % pmc_l_vc   = obj.get_p_minus_c_from_xy([h_range(1) obj.vc]);
+            % pmc_r_vc   = obj.get_p_minus_c_from_xy([h_range(2) obj.vc]);
+            % pmc_top_hc = obj.get_p_minus_c_from_xy([obj.hc v_range(1)]);
+            % pmc_btm_hc = obj.get_p_minus_c_from_xy([obj.hc v_range(2)]);
+            % pmc_h = [pmc_l_vc' pmc_r_vc'];
+            % pmc_v = [pmc_top_hc' pmc_btm_hc'];
+        end
+        
+        function [fovh,fovv] = get_FOV(obj,h_range,v_range)
+            [pmc_h,pmc_v] = obj.get_pmc_FOV(h_range,v_range);
+            fovl = acos(obj.A*pmc_h(:,1));
+            fovr = acos(obj.A*pmc_h(:,2));
+            fovt = acos(obj.A*pmc_v(:,1));
+            fovb = acos(obj.A*pmc_v(:,2));
+           
+            fovh = [fovl,fovr];
+            fovv = [fovt;fovb];
+        end
+
+        function [pmc_ul,pmc_ll,pmc_lr,pmc_ur] = get_pmc_FOVvertex(obj,h_range,v_range)
+            obj.get_image_plane_unit_vectors();
+            pmc_ul_counter_clock   = obj.get_p_minus_c_from_xy(...
+                [ h_range(1) v_range(1);
+                  h_range(1) v_range(2);
+                  h_range(2) v_range(1);
+                  h_range(2) v_range(2) ]');
+            pmc_ul = pmc_ul_counter_clock(:,1);
+            pmc_ll = pmc_ul_counter_clock(:,2);
+            pmc_lr = pmc_ul_counter_clock(:,3);
+            pmc_ur = pmc_ul_counter_clock(:,4);
+        end
+        
+        % function [fovh,fovv] = get_FOV(obj,h_range,v_range)
+        %     obj.get_optical_center_pixel();
+        %     pmc_l_vc = cahv_get_p_minus_c_from_xy([h_range(1) obj.vc],obj);
+        %     pmc_r_vc = cahv_get_p_minus_c_from_xy([h_range(2) obj.vc],obj);
+        %     pmc_top_hc = cahv_get_p_minus_c_from_xy([obj.hc v_range(1)],obj);
+        %     pmc_btm_hc = cahv_get_p_minus_c_from_xy([obj.hc v_range(2)],obj);
+        %     fovl = acos(pmc_l_vc*obj.A');
+        %     fovr = acos(pmc_r_vc*obj.A');
+        %     fovt = acos(pmc_top_hc*obj.A');
+        %     fovb = acos(pmc_btm_hc*obj.A');
+        %    
+        %     fovh = [fovl,fovr];
+        %     fovv = [fovt;fovb];
+        %end
 
     end
 end
