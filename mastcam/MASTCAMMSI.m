430d28fbf3e4d5496487822e6b70b2d0633ee656
Yuki Itoh <yukiitohand@gmail.com>
2020-09-28 13:10:41 -0400

    hdr is added, 'C' is now hondled

--- mastcam/MASTCAMMSI.m
+++ mastcam/MASTCAMMSI.m
@@ -13,6 +13,7 @@ classdef MASTCAMMSI < dynamicprops
         img_iof
         eye
         radiance_factor
+        hdr
     end
     
     methods
@@ -24,7 +25,7 @@ classdef MASTCAMMSI < dynamicprops
                     obj.filternames = {'L0Red','L0Green','L0Blue','L1','L2','L3','L4','L5','L6'};
                 case 'R'
                     obj.eye = 'R';
-                    [obj.filter_info] = mastcamL_get_filter_info();
+                    [obj.filter_info] = mastcamR_get_filter_info();
                      obj.filternames = {'R0Red','R0Green','R0Blue','R1','R2','R3','R4','R5','R6'};
             end
             addprop(obj,[obj.eye,'0']);
@@ -39,6 +40,9 @@ classdef MASTCAMMSI < dynamicprops
             if any(strcmpi(DATA_PROC_CODE,{'DRLX','DRXX'}))
                 obj.set_imgIoF(mstdata_seq);
             end
+            obj.img = obj.img_iof;
+            obj.MASTCAMgroup_eye = mstdata_seq;
+            obj.set_header();
 
         end
         
@@ -51,9 +55,25 @@ classdef MASTCAMMSI < dynamicprops
             obj.wavelength_filternames = filternames_sorted;
         end
         
+        function set_header(obj)
+            obj.hdr = [];
+            if isprop(obj.MASTCAMgroup_eye,'E')
+                obj.hdr.samples = obj.MASTCAMgroup_eye.E.DRCL.hdr.samples;
+                obj.hdr.lines = obj.MASTCAMgroup_eye.E.DRCL.hdr.lines;
+            elseif isprop(obj.MASTCAMgroup_eye,'C')
+                obj.hdr.samples = obj.MASTCAMgroup_eye.C.DRCL.hdr.samples;
+                obj.hdr.lines = obj.MASTCAMgroup_eye.C.DRCL.hdr.lines;
+            end
+            obj.hdr.bands = size(obj.img,3);
+        end
+        
         function set_img(obj,mstdata_seq)
             % load images from mstdat_seq
-            imRGB = mstdata_seq.E.(obj.DATA_PROC_CODE).readimg();
+            if isprop(mstdata_seq,'E')
+                imRGB = mstdata_seq.E.(obj.DATA_PROC_CODE).readimg();
+            elseif isprop(mstdata_seq,'C')
+                imRGB = mstdata_seq.C.(obj.DATA_PROC_CODE).readimg();
+            end
             obj.([obj.eye '0']) = imRGB;
             obj.([obj.eye '0Red']) = imRGB(:,:,1);
             obj.([obj.eye '0Green']) = imRGB(:,:,2);
@@ -78,11 +98,19 @@ classdef MASTCAMMSI < dynamicprops
         function set_imgIoF(obj,mstdata_seq)
             obj.radiance_factor = [];
             iof = [];
-            imRGB_iof = mstdata_seq.E.(obj.DATA_PROC_CODE).get_IoF();
+            if isprop(mstdata_seq,'E')
+                imRGB_iof = mstdata_seq.E.(obj.DATA_PROC_CODE).get_IoF();
+            elseif isprop(mstdata_seq,'C')
+                imRGB_iof = mstdata_seq.C.(obj.DATA_PROC_CODE).get_IoF();
+            end
             iof.([obj.eye '0Red']) = imRGB_iof(:,:,1);
             iof.([obj.eye '0Green']) = imRGB_iof(:,:,2);
             iof.([obj.eye '0Blue']) = imRGB_iof(:,:,3);
-            obj.radiance_factor.([obj.eye '0']) = mstdata_seq.E.(obj.DATA_PROC_CODE).RADIANCE_FACTOR;
+            if isprop(mstdata_seq,'E')
+                obj.radiance_factor.([obj.eye '0']) = mstdata_seq.E.(obj.DATA_PROC_CODE).RADIANCE_FACTOR;
+            elseif isprop(mstdata_seq,'C')
+                obj.radiance_factor.([obj.eye '0']) = mstdata_seq.C.(obj.DATA_PROC_CODE).RADIANCE_FACTOR;
+            end
             
             for i=1:length(mstdata_seq.D.(obj.DATA_PROC_CODE))
                 filter_number = mstdata_seq.D.(obj.DATA_PROC_CODE)(i).FILTER_NUMBER;
@@ -96,14 +124,15 @@ classdef MASTCAMMSI < dynamicprops
                 if i==1
                     obj.img_iof = iof.(obj.wavelength_filternames{i});
                 else
-                    obj.img_iof = cat(3,obj.img,iof.(obj.wavelength_filternames{i}));
+                    obj.img_iof = cat(3,obj.img_iof,iof.(obj.wavelength_filternames{i}));
                 end
             end
         end
         
-        function [spc,wv] = get_spectrum(obj,s,l,varargin)
+        function [spc,wv,bdxes] = get_spectrum(obj,s,l,varargin)
             spc = squeeze(obj.img(l,s,:));
             wv = obj.wavelength;
+            bdxes = 1:obj.hdr.bands;
         end
         
     end
