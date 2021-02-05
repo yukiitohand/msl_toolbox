013df1b60c480814ccff63dc522f99d878ceb628
Yuki Itoh <yukiitohand@gmail.com>
2021-02-05 14:13:23 -0500

    fields x and y for hdr property is removed:

--- util/mslGaleMosaic/MSLGaleMosaic_v3_lbl2hdr.m
+++ util/mslGaleMosaic/MSLGaleMosaic_v3_lbl2hdr.m
@@ -43,13 +43,4 @@ map_info.units = 'Meters';
 
 hdr_info.map_info = map_info;
 
-xi = 1:hdr_info.samples;
-yi = 1:hdr_info.lines;
-
-x = map_info.dx*(xi-map_info.image_coords(1))+map_info.mapx;
-y = -map_info.dy*(yi-map_info.image_coords(2))+map_info.mapy;
-
-hdr_info.x = x;
-hdr_info.y = y;
-
 end
