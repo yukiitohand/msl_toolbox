874f55e45a26849a867b79538ddb9ab6ea534f27
Yuki Itoh <yukiitohand@gmail.com>
2021-06-16 19:53:27 -0400

    bug regarding ENVI map_info is fixed

--- util/mslGaleMosaic/MSLGaleMosaic_v3_lbl2hdr.m
+++ util/mslGaleMosaic/MSLGaleMosaic_v3_lbl2hdr.m
@@ -33,7 +33,11 @@ hdr_info.data_ignore_value = -32767;
 % next, loading mapping information
 map_info = [];
 map_info.projection = lbl_info.OBJECT_IMAGE_MAP_PROJECTION.MAP_PROJECTION_TYPE;
-map_info.image_coords = [1 1];
+% [1,1] is considered as the center of the most upper left pixel by the 
+% class SphereEquiRectangularProj, while in ENVI, [1.5 1.5] is considered 
+% as the center of the most upper left pixel. [1 1] is the upper left
+% vertex of the upper left most pixel.
+map_info.image_coords = [1.5 1.5];
 map_info.mapx = -lbl_info.OBJECT_IMAGE_MAP_PROJECTION.SAMPLE_PROJECTION_OFFSET.value*lbl_info.OBJECT_IMAGE_MAP_PROJECTION.MAP_SCALE.value;
 map_info.mapy = lbl_info.OBJECT_IMAGE_MAP_PROJECTION.LINE_PROJECTION_OFFSET.value*lbl_info.OBJECT_IMAGE_MAP_PROJECTION.MAP_SCALE.value;
 map_info.dx   = lbl_info.OBJECT_IMAGE_MAP_PROJECTION.MAP_SCALE.value;
@@ -42,5 +46,10 @@ map_info.datum = 'D_Mars_2000_Sphere';
 map_info.units = 'Meters';
 
 hdr_info.map_info = map_info;
+
+[coordinate_system_string] = mslGaleMosaic_v3_get_coordinate_system_string();
+hdr_info.coordinate_system_string = coordinate_system_string;
+hdr_info.coordinate_system_struct = wkt2struct(coordinate_system_string(2:end-1));
+
 
 end
