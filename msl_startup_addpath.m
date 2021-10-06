23bf7fc769c0ec50ee0e3f144d13344ca39fe9c3
Yuki Itoh <yukiitohand@gmail.com>
2021-10-06 15:25:18 -0400

    bugs are fixed

--- msl_startup_addpath.m
+++ msl_startup_addpath.m
@@ -4,8 +4,8 @@ function [] = msl_startup_addpath()
 fpath_self = mfilename('fullpath');
 [dirpath_self,filename] = fileparts(fpath_self);
 
-idx_sep = strfind(dirpath_self,'msl_toolbox/setting');
-toolbox_root_dir = dirpath_self(1:idx_sep-1);
+mtch = regexpi(dirpath_self,'(?<parent_dirpath>.*)/msl_toolbox[/]{0,1}','names');
+toolbox_root_dir = mtch.parent_dirpath;
 
 
 %-------------------------------------------------------------------------%
@@ -17,9 +17,10 @@ crism_toolbox_dirname         = 'crism_toolbox';
 msl_toolbox_dirname           = 'msl_toolbox';
 
 %-------------------------------------------------------------------------%
+pathCell = strsplit(path, pathsep);
 %%
 % base toolbox
-base_toolbox_dir = [toolbox_root_dir base_toolbox_dirname '/'];
+base_toolbox_dir = [toolbox_root_dir '/' base_toolbox_dirname '/'];
 addpath(base_toolbox_dir);
 % joinPath in base toolbox will be used in the following. "base" toolbox
 % need to be loaded first. base/joinPath.m automatically determine the
@@ -41,7 +42,7 @@ envi_toolbox_dir = joinPath(toolbox_root_dir, envi_toolbox_dirname);
 
 if exist(envi_toolbox_dir,'dir')
     if ~check_path_exist(envi_toolbox_dir, pathCell)
-        run(joinPath(envi_toolbox_dir,'envi_startup_path'));
+        run(joinPath(envi_toolbox_dir,'envi_startup_addpath'));
     end
 else
     warning([ ...
@@ -55,7 +56,7 @@ pds3_toolbox_dir = joinPath(toolbox_root_dir, pds3_toolbox_dirname);
 
 if exist(pds3_toolbox_dir,'dir')
     if ~check_path_exist(pds3_toolbox_dir, pathCell)
-        run(joinPath(pds3_toolbox_dir,'pds3_startup_path'));
+        run(joinPath(pds3_toolbox_dir,'pds3_startup_addpath'));
     end
 else
     warning([ ...
@@ -69,7 +70,7 @@ end
 crism_toolbox_dir = joinPath(toolbox_root_dir, crism_toolbox_dirname);
 if exist(crism_toolbox_dir,'dir')
     if ~check_path_exist(crism_toolbox_dir, pathCell)
-        run(joinPath(crism_toolbox_dir,'crism_startup_path'));
+        run(joinPath(crism_toolbox_dir,'crism_startup_addpath'));
     end
 else
     warning([ ...
